b7b4743b746da8a011124b53a40f8d10112f387d
Ahmet <ahmetbasaran@live.com>
2019-04-28 19:34:05 -0400

    initial

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var arr = [
   {
     'guest_type': 'crew',
@@ -55,18 +56,66 @@ var arr = [
   },
 ];
 
-function mutateArray(a) {
-    console.log('a flat', a.flat());
-    return a.flat();
+// challenge # 1
+function mutateFlatArray(objToModify, nestedObj) {
+  // dot notate into child as passed in (could be replaced with objToModify.guest_booking)
+  const child = objToModify[nestedObj];
+
+  //coalesce - this has our original object, and the nested object values 
+  const flatObj = {...objToModify, ...child};
+
+  //delete our nestedObj to avoid duplicates
+  delete flatObj[nestedObj];
+  return flatObj;
+};
+
+// challenge # 2
+function someTotalArray(objToModify) {
+  // make sure it exists
+  if(objToModify) {
+    // iterate through and do our transformation
+      // reduce to get sum
+      var numTotals = objToModify.some_array.reduce(function(sum, value) {
+        return sum + value;
+      });
+      // rename prop
+      objToModify.some_total = numTotals;
+      delete objToModify.some_array;
+      return objToModify;
+  }
+  testVar = objToModify;
+  return objToModify;
 }
 
-function flatTest() {
-   var arr1= [1, 2, [3,4]];
-   console.log('flat', arr1.flat());
-}
+
+const flatArr = arr.map(input => mutateFlatArray(input, 'guest_booking'));
+//spread operator deep copy - otherwise both challenge #2 and #3 arrays are modified (this was a pain to figure out - lots of fun google)
+const clonedFlat = flatArr.map(x => ({...x}));
+const totalArr = clonedFlat.map(input => someTotalArray(input));
+// just used js filter
+const filteredArr = clonedFlat.filter(input => input.guest_type === 'guest');
+// just used js sort
+
+// another spread operator deep copy to maintain integrity of challenge # 3
+const clonedFiltered = filteredArr.map(y => ({...y}));
+
+const alphaOrderArr = clonedFiltered.sort(function(a, b) {
+  if (a.last_name === b.last_name) {
+    if(a.first_name < b.first_name) {
+      return -1;
+    }
+  }
+  if(a.last_name < b.last_name) { return -1 };
+  if(a.last_name > b.last_name) { return 1 };
+  return 0;
+});
+
+console.log('alphaOrderArr', alphaOrderArr);
 
 $(document).ready(function() {
-    $('#originalArray').html(JSON.stringify(arr, null, 2));
-    $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
-    flatTest();
+  $('#originalArray').html(JSON.stringify(arr, null, 2));
+  $('#flatResultsArray').html(JSON.stringify(flatArr, null, 2));
+  $('#someTotalResultsArray').html(JSON.stringify(totalArr, null, 2));
+  $('#filterGuestResultsArray').html(JSON.stringify(filteredArr, null, 2));
+  $('#sortAlphaResultsArray').html(JSON.stringify(alphaOrderArr, null, 2));
 });
